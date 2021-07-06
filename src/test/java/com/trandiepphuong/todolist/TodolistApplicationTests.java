af5f7af2dc675f3aa56eae118328b8d041480430
phuong15032000 <52631881+phuong15032000@users.noreply.github.com>
2021-07-06 10:12:16 +0700

    config-codecov

--- src/test/java/com/trandiepphuong/todolist/TodolistApplicationTests.java
+++ src/test/java/com/trandiepphuong/todolist/TodolistApplicationTests.java
@@ -1,13 +1,16 @@
 package com.trandiepphuong.todolist;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
 class TodolistApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    public void contextLoads() {
+    }
 }
